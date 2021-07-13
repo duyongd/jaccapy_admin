beaf42e315a82a2a7f7e190c011f1143b8662902
user <user@DESKTOP-088CSE7>
2021-07-13 20:01:15 +0900

    첫번째 커밋

---
+++ sample/src/main/java/jcappy/jcappyController.java
@@ -0,0 +1,55 @@
+package jcappy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class jcappyController {
+		
+//	@RequestMapping("/jcappy/admin.do") // 로그인 화면
+//	public String index() {
+//		return "admin/jcappy/index";
+//	}
+	
+//	@RequestMapping("/jcappy/account/index.do") // 관리자 계정 관리
+//	public String account() {
+//		return "admin/jcappy/account/index";
+//	}
+	
+//	@RequestMapping("/jcappy/account/write.do") // 관리자 계정 관리
+//	public String accountWrite() {
+//		return "admin/jcappy/account/write";
+//	}
+//	
+//	@RequestMapping("/jcappy/account/view.do") // 관리자 계정 상세 보기
+//	public String accountView() {
+//		return "admin/jcappy/account/view";
+//	}
+	
+//	@RequestMapping("/jcappy/user/index.do") // 회원 목록
+//	public String user() {
+//		return "admin/jcappy/user/index";
+//	}
+//	
+//	@RequestMapping("/jcappy/user/view.do") // 회원 상세
+//	public String userView() {
+//		return "admin/jcappy/user/view";
+//	}
+
+//	@RequestMapping("/jcappy/item/index.do") // 상품 목록
+//	public String item() {
+//		return "admin/jcappy/item/index";
+//	}
+//	
+//	@RequestMapping("/jcappy/item/view.do") // 상품 상세
+//	public String itemView() {
+//		return "admin/jcappy/item/view";
+//	}
+//	
+//	@RequestMapping("/jcappy/item/write.do") // 상품 등록
+//	public String itemWrite() {
+//		return "admin/jcappy/item/write";
+//	}
+	
+	
+}
